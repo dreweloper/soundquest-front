54f711b19c2bc8a0fa4c619c596e2ec863e50319
dreweloper <dreweloper@gmail.com>
2023-06-29 13:49:20 +0200

    Limpio HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,24 +1,5 @@
-import { useEffect, useState } from "react";
-import { fetchAPI } from "../api/fetchAPI";
 
 export const HomePage = () => {
-
-    const [datos, setDatos] = useState()
-
-
-    useEffect(() => {
-
-        const fetch = async () => {
-
-            const token = await fetchAPI('https://soundquest-xf5r.onrender.com/api/v1/token');
-
-            setDatos(token);
-
-        };
-
-        fetch();
-
-    }, []);
 
 
     return (
@@ -26,8 +7,6 @@ export const HomePage = () => {
         <>
 
             <h1> SoundQuest </h1>
-
-            <p> {JSON.stringify(datos)} </p>
 
         </>
 
