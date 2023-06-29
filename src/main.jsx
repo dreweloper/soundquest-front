bb61fc443222811359163d54a5b5b1724500b9af
dreweloper <dreweloper@gmail.com>
2023-06-29 12:47:51 +0200

    Instalo react-router-dom y config. general. Creo carpeta routes y pages, archivos AppRouter y HomePage. Todo funcionando.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('app')).render(
 
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 
 );
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+
+export const HomePage = () => {
+
+
+    return (
+
+        <>
+
+            <h1> SoundQuest </h1>
+
+        </>
+
+    );
+};
