696b753d61aafa0bc602ec4cf537b9fff105f709
dreweloper <dreweloper@gmail.com>
2023-07-19 22:56:04 +0200

    Creo reducers para limpiar los estados de token, playlist y track. Hago el dispatch de todos en HomePage. Todo funcionando correctamente.

--- src/store/slices/playlistSlice.js
+++ src/store/slices/playlistSlice.js
@@ -13,10 +13,14 @@ export const playlistSlice = createSlice({
         setPlaylist: (state, { payload }) => {
             state.playlist_id = payload.playlist_id;
             state.playlist_url = payload.playlist_url;
+        },
+        clearPlaylist: (state) => {
+            state.playlist_id = undefined;
+            state.playlist_url = undefined;
         }
     }
 
 });
 
 
-export const { setPlaylist } = playlistSlice.actions;
+export const { setPlaylist, clearPlaylist } = playlistSlice.actions;
