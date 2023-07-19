72e456e616fa09b1082d4087e476af941f648a63
dreweloper <dreweloper@gmail.com>
2023-07-19 22:25:45 +0200

    Añado footer con logos S y XL según media-query en DiscoverPage. Todo OK.

--- src/pages/DiscoverPage.jsx
+++ src/pages/DiscoverPage.jsx
@@ -108,6 +108,18 @@ export const DiscoverPage = () => {
 
             </main>
 
+            <footer className='footerDiscover'>
+
+                <div className='spotifyLogo'>
+
+                    <img className='logoS' src="/assets/spotify/icons/Spotify_Icon_RGB_Green.png" alt="Spotify logo" title='Spotify logo' />
+
+                    <img className='logoXL' src="/assets/spotify/logos/Spotify_Logo_RGB_Green.png" alt="Spotify logo" title='Spotify logo' />
+
+                </div>
+
+            </footer>
+
         </>
 
     );
