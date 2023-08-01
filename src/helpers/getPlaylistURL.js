11bce6f6434ce45241693fbf557c57798293dcc4
dreweloper <dreweloper@gmail.com>
2023-08-01 19:23:04 +0200

    Documentación de helpers done.

--- src/helpers/getPlaylistURL.js
+++ src/helpers/getPlaylistURL.js
@@ -1,12 +1,13 @@
 /**
- * This function filters the array to get the playlist object and then maps it to get the Spotify URL of the playlist.
+ * Filters the array to get the playlist object and then maps it to get the Spotify URL of the playlist.
+ * 
  * @function getPlaylistURL
- * @param {Array} arr It contains an object for each user's playlist.
- * @param {String} id The random playlist ID returned by the function 'randomPlaylist'.
- * @returns {String} The Spotify's playlist URL.
+ * @param {Array.<Object>} arr - An array containing objects for each user's playlist.
+ * @param {String} id - The random playlist ID returned by the function 'randomPlaylist'.
+ * @returns {String} - The Spotify URL of the playlist.
  */
 export const getPlaylistURL = (arr, id) => {
 
     return arr.filter(item => item.id == id).map(item => item.external_urls.spotify).toString();
 
-}; //!GETPLAYLISTURL
+};
