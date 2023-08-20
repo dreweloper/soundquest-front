ab5273512447eb61b9c2b01e11ef41eee0d6adef
dreweloper <dreweloper@gmail.com>
2023-08-20 12:17:57 +0200

    Implementada lógica en elementos finales de componente InfoBox. Pendiente estilos.

--- src/components/InfoBox.jsx
+++ src/components/InfoBox.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 export const InfoBox = ({ isInfoBoxOpen, setIsInfoBoxOpen }) => {
+
+    const [language, setLanguage] = useState('EN');
 
 
     return (
@@ -11,6 +14,24 @@ export const InfoBox = ({ isInfoBoxOpen, setIsInfoBoxOpen }) => {
             <div className='overlay'>
 
                 <section className='info-box'>
+
+                    <div className='language-options'>
+
+                        <span
+                            className={language == 'EN' ? 'font-bold' : null}
+                            onClick={() => { setLanguage('EN')}}
+                        >
+                            EN
+                        </span>
+                        |
+                        <span
+                            className={language == 'ES' ? 'font-bold' : null}
+                            onClick={() => { setLanguage('ES')}}
+                        >
+                            ES
+                        </span>
+
+                    </div>
 
                     <button
                         className='close-button'
@@ -23,7 +44,13 @@ export const InfoBox = ({ isInfoBoxOpen, setIsInfoBoxOpen }) => {
 
                     </button>
 
-                    <p className='info-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quo adipisci modi corporis officia, debitis natus deserunt quibusdam incidunt! Omnis accusantium nam dolor animi fuga voluptas rerum harum error molestias cumque molestiae facere magni voluptatum aliquam magnam sit doloribus sequi ut, quidem autem ad ullam et consequuntur veniam. Id provident impedit quam maiores modi quod dolor at, earum vitae. Asperiores, possimus? Adipisci ducimus eligendi aliquid maxime debitis libero delectus deleniti, dolor dolorum ab numquam, quod dolores ratione provident laborum at sint temporibus molestias eum? Autem, velit recusandae reiciendis tenetur saepe illo voluptas magnam officia eius iste consequatur quaerat cumque nam.</p>
+                    {
+                        language == 'EN' && (<p className='info-text'>SoundQuest isn't just an app, it's your gateway to a universe of new music discovery. Leveraging Spotify's API, we cracked the code to bring you truly random tracks, sparking your musical journey like never before. What started as a way to share my own playlists evolved into something even greater. Now, you have the power to be the host, inviting yourself or a friend into the mix, or even tapping into Spotify's official user ID to explore over 1,300 professionally curated playlists. Click, explore, and embark on a limitless sonic journey with SoundQuest’s one-click wonder, right from where you are!</p>)
+                    }
+
+                    {
+                        language == 'ES' && (<p className='info-text'>SoundQuest no es solo una aplicación, es tu entrada a un universo de música nueva. El resultado de combinar la API de Spotify y nuestro algoritmo es que ahora puedes disfrutar de una experiencia musical con canciones completamente aleatorias. Lo que comenzó como una forma de compartir mis propias playlists evolucionó en algo mucho más significativo. Ahora, tienes el poder de ser el anfitrión, ya sea invitándote a ti mismo o a un amigo, o incluso conectándote con el usuario oficial de Spotify para explorar más de 1.300 playlists curadas por profesionales. ¡Haz click, explora y embárcate en un viaje sonoro sin límites desde donde estés con SoundQuest!</p>)
+                    }
 
                     <Link className='github-link' to='https://github.com/dreweloper' target='_blank'>
 
